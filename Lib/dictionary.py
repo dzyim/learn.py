b2849b31cc6128021b5bc3eb5b9f416c808316b2
liangchen <ioliangchen@163.com>
2023-06-15 16:30:25 +0800

    Create dictionary.py

---
+++ Lib/dictionary.py
@@ -0,0 +1,43 @@
+__all__ = ['AttrDict']
+
+
+class AttrDict:
+    '''Make dictionary items as object attributes.'''
+    
+    def __init__(self, **kwargs):
+        for key in kwargs:
+            self[key] = kwargs[key]
+    
+    def __getitem__(self, key):
+        val = vars(self)[key]
+        if isinstance(val, (tuple, list, set)):
+            return type(val)(i.todict() if isinstance(i, AttrDict) else i for i in val)
+        else:
+            return val.todict() if isinstance(val, AttrDict) else val
+    
+    def __iter__(self):
+        return iter(vars(self))
+    
+    def __len__(self):
+        return len(vars(self))
+    
+    def __repr__(self):
+        L = ('%s: %s' % (repr(k), repr(v)) for k, v in self.items())
+        return 'AttrDict({%s})' % ', '.join(L)
+    
+    def __setitem__(self, key, val):
+        if isinstance(val, (tuple, list, set)):
+            L = type(val)(AttrDict(**i) if isinstance(i, dict) else i for i in val)
+            setattr(self, key, L)
+        else:
+            setattr(self, key, AttrDict(**val) if isinstance(val, dict) else val)
+    
+    def get(self, key):
+        return vars(self).get(key)
+    
+    def items(self):
+        return vars(self).items()
+    
+    def todict(self):
+        return dict((key, self[key]) for key in self)
+    
