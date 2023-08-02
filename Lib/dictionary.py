a516fe8a857c86f240692a548b05949a754fc95a
liangchen <ioliangchen@163.com>
2023-08-02 17:35:06 +0800

    Update Lib/dictionary.py

--- Lib/dictionary.py
+++ Lib/dictionary.py
@@ -5,6 +5,8 @@ from collections.abc import Mapping
 class AttrDict(Mapping):
     '''Make dictionary items as object attributes.'''
     
+    force = False
+    
     def __init__(self, **kwargs):
         for key in kwargs:
             self[key] = kwargs[key]
@@ -15,9 +17,9 @@ class AttrDict(Mapping):
     def __getitem__(self, key):
         val = vars(self)[key]
         if isinstance(val, (tuple, list, set)):
-            return type(val)(i.todict() if isinstance(i, AttrDict) else i for i in val)
+            return type(val)(i.to_dict() if isinstance(i, AttrDict) else i for i in val)
         else:
-            return val.todict() if isinstance(val, AttrDict) else val
+            return val.to_dict() if isinstance(val, AttrDict) else val
     
     def __iter__(self):
         return iter(vars(self))
@@ -31,11 +33,23 @@ class AttrDict(Mapping):
     
     def __setitem__(self, key, val):
         if isinstance(val, (tuple, list, set)):
-            L = type(val)(AttrDict(**i) if isinstance(i, dict) else i for i in val)
+            L = type(val)(AttrDict.from_dict(i, AttrDict.force) if isinstance(i, dict) else i for i in val)
             setattr(self, key, L)
         else:
-            setattr(self, key, AttrDict(**val) if isinstance(val, dict) else val)
+            setattr(self, key, AttrDict.from_dict(val, AttrDict.force) if isinstance(val, dict) else val)
     
-    def todict(self):
+    @classmethod
+    def from_dict(cls, dct, force=False):
+        cls.force = force
+        if cls.force:
+            dct = {str(key): dct[key] for key in dct}
+        new = cls(**dct)
+        cls.force = False
+        return new
+    
+    def to_dict(self):
         return dict((key, self[key]) for key in self)
+    
+    fromdict = fromDict = from_dict
+    todict = toDict = to_dict
     
