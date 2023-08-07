aa6343f642bc108606c3e6457c584db8b17402bd
liangchen <ioliangchen@163.com>
2023-08-07 18:00:25 +0800

    Update Algo/root.py

--- Algo/root.py
+++ Algo/root.py
@@ -4,7 +4,14 @@ https://en.wikipedia.org/wiki/Category:Root-finding_algorithms
 https://www.composingprograms.com/pages/16-higher-order-functions.html
 '''
 
-__all__ = ['approx_eq', 'improve', 'sqrt']
+__all__ = [
+    'approx_eq',
+    'improve',
+    'sqrt',
+    'newton_update',
+    'find_zero',
+    'nth_root',
+]
 
 def approx_eq(x, y, tolerance=1e-3):
     return abs(x - y) < tolerance
@@ -14,15 +21,32 @@ def improve(update, close, guess=1.0):
         guess = update(guess)
     return guess
 
-def sqrt(a, tolerance=1e-3):
+def sqrt(a):
     def sqrt_update(x):
         return (x + a/x) / 2
     def sqrt_close(x):
-        return approx_eq(x, a/x, tolerance)
+        return approx_eq(x, a/x)
     if a < 0:
-        return 1j * sqrt(-a, tolerance)
+        return 1j * sqrt(-a)
     elif a == 0:
         return 0
     else:
         return improve(sqrt_update, sqrt_close)
+
+def newton_update(f, df):
+    def update(x):
+        return x - f(x) / df(x)
+    return update
+
+def find_zero(f, df):
+    def close(x):
+        return approx_eq(f(x), 0)
+    return improve(newton_update(f, df), close)
+
+def nth_root(radicand, index):
+    def f(x):
+        return x**index - radicand
+    def df(x):
+        return index * x**(index-1)
+    return find_zero(f, df)
     
